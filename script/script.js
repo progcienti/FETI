3feefa7978bc793e576383e2e5c0b10c22a7bec3
joão.jonny <138599648+progcienti@users.noreply.github.com>
2025-02-01 17:02:02 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,4 @@
+document.getElementById("hamburger").addEventListener("click", function() {
+    var menuLinks = document.getElementById("menuLinks");
+    menuLinks.classList.toggle("active");
+});
